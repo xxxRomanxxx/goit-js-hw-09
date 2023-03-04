f8ffb3ae927189ff3a4751b14b7f0b601917960c
xxxRomanxxx <112776664+xxxRomanxxx@users.noreply.github.com>
2023-03-04 23:11:33 +0200

    Task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+let isActive = false;
+
+
+buttonStart.addEventListener('click', onButtonStartClick);
+buttonStop.addEventListener('click', onButtonStopClick)
+
+function onButtonStartClick() {    
+    if (isActive) {
+        return;
+    }
+    isActive = true;
+    timerId = setInterval(() => { 
+        body.style.backgroundColor = getRandomHexColor()}, 1000)
+};
+
+function onButtonStopClick() {
+    clearInterval(timerId);
+    isActive = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
