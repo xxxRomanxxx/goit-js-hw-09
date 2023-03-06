7499f5e1254710910a5d088859cd819c44ba3215
xxxRomanxxx <112776664+xxxRomanxxx@users.noreply.github.com>
2023-03-06 23:44:20 +0200

    task 3 start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const inputFirstDelay= document.querySelector('[name="delay"]');
+const inputStep = document.querySelector('[name="step"]');
+const inputAmount = document.querySelector('[name="amount"]');
+const buttonSubmit = document.querySelector('button');
+
+buttonSubmit.addEventListener('click', onButtonSubmit);
+
+function onButtonSubmit() {
+  function createPromise(position, delay) {
+    return new Promise((resolve, reject)=>{
+      setInterval(()=>{
+        const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(position, delay)
+      } else {
+        reject(position, delay)
+      }
+      }, inputFirstDelay.value)   
+    }) 
   }
+  
+  createPromise(2, 1500)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
+
