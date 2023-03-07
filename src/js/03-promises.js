22f87867ee04f3b510b361118c42e4a321dd9ae1
xxxRomanxxx <112776664+xxxRomanxxx@users.noreply.github.com>
2023-03-07 20:45:41 +0200

    Task-3 complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,30 +1,39 @@
-const inputFirstDelay= document.querySelector('[name="delay"]');
-const inputStep = document.querySelector('[name="step"]');
-const inputAmount = document.querySelector('[name="amount"]');
-const buttonSubmit = document.querySelector('button');
+import Notiflix from 'notiflix';
 
-buttonSubmit.addEventListener('click', onButtonSubmit);
+const formDelay = document.querySelector('[name="delay"]');
+const formStep = document.querySelector('[name="step"]');
+const formAmount = document.querySelector('[name="amount"]');
+const formSubmit = document.querySelector('button');
 
-function onButtonSubmit() {
-  function createPromise(position, delay) {
-    return new Promise((resolve, reject)=>{
-      setInterval(()=>{
-        const shouldResolve = Math.random() > 0.3;
+formSubmit.addEventListener('click', onSubmitForm);
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-        resolve(position, delay)
+        resolve({position, delay})
       } else {
-        reject(position, delay)
+        reject({position, delay})
       }
-      }, inputFirstDelay.value)   
-    }) 
-  }
-  
-  createPromise(2, 1500)
+      }, delay)
+  })
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  for (let i = 0; i < Number(formAmount.value); i += 1) {
+    createPromise(i, Number(formDelay.value) + Number(formStep.value) * i)
     .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
     })
     .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
     });
-}
+  }
+};
+
+
+
 
